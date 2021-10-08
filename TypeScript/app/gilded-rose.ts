81666fdb782910e9c530cad95f8a471099c64f11
Amedee Van Gasse <amedee-github@amedee.be>
2021-10-08 16:49:45 +0200

    Extract conditional backstage passes

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -37,6 +37,40 @@ export class GildedRose {
                 }
             }
         } else {
+            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                this.foo(item);
+            } else {
+                this.foo(item);
+            }
+        }
+    }
+
+    private foo(item: Item) {
+        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.quality > 0) {
+                if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                    item.quality = item.quality - 1
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+        }
+        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.sellIn = item.sellIn - 1;
+        }
+        if (item.sellIn < 0) {
             if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (item.quality > 0) {
                     if (item.name != 'Sulfuras, Hand of Ragnaros') {
@@ -44,35 +78,7 @@ export class GildedRose {
                     }
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.sellIn = item.sellIn - 1;
-            }
-            if (item.sellIn < 0) {
-                if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.quality > 0) {
-                        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                            item.quality = item.quality - 1
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality
-                }
+                item.quality = 0
             }
         }
     }
