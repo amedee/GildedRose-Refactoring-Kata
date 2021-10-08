5c6bd4e24e9e2d24deb39ee7426f53bdb7837cfb
Amedee Van Gasse <amedee-github@amedee.be>
2021-10-08 16:44:08 +0200

    Inline and simplify Aged Brie

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -27,41 +27,43 @@ export class GildedRose {
 
     private doUpdateQuality(item: Item) {
         if (item.name == 'Aged Brie') {
-            this.foo(item);
-        } else {
-            this.foo(item);
-        }
-    }
-
-    private foo(item: Item) {
-        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-                if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                    item.quality = item.quality - 1
-                }
-            }
-        } else {
             if (item.quality < 50) {
                 item.quality = item.quality + 1
-                if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
+            }
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        } else {
+            if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.quality > 0) {
+                    if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                        item.quality = item.quality - 1
                     }
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
+                            }
+                        }
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
+                            }
                         }
                     }
                 }
             }
-        }
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.sellIn = item.sellIn - 1;
-        }
-        if (item.sellIn < 0) {
-            if (item.name != 'Aged Brie') {
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                item.sellIn = item.sellIn - 1;
+            }
+            if (item.sellIn < 0) {
                 if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                     if (item.quality > 0) {
                         if (item.name != 'Sulfuras, Hand of Ragnaros') {
@@ -70,10 +72,6 @@ export class GildedRose {
                     }
                 } else {
                     item.quality = item.quality - item.quality
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
                 }
             }
         }
