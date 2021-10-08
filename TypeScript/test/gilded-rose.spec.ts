f10d5efe0c22a2cf0b993d3e1b7dbb31b6bc5fa3
Amedee Van Gasse <amedee-github@amedee.be>
2021-10-08 16:05:41 +0200

    Increase coverage

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -12,11 +12,16 @@ describe('Gilded Rose', () => {
     it('should update quality', () =>
         expect(doUpdateQuality).toVerifyAllCombinations(
             [
-                'foo'
+                'foo',
+                'Aged Brie',
+                'Backstage passes to a TAFKAL80ETC concert',
+                'Sulfuras, Hand of Ragnaros',
+                'Conjured Mana Cake'
             ],
-            [-1],
-            [0]
-        ));
+            [-1, 0, 1, 5, 6, 7, 10, 11, 12],
+            [0, 1, 49, 50]
+        )
+    );
 
     it('should be verified with golden master', () =>
         expect(goldenMaster()).toMatchSnapshot());
