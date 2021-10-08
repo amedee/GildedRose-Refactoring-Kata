48e61641c42082f1e2d297882fd05ff9c5ee0af6
Amedee Van Gasse <amedee-github@amedee.be>
2021-10-08 17:10:16 +0200

    Inline and simplify Sulfuras

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -55,29 +55,17 @@ export class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0
                 }
+            } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
+                // Nothing to do!
             } else {
-                if (item.name == 'Sulfuras, Hand of Ragnaros') {
-                    this.foo(item);
-                } else {
-                    this.foo(item);
-                }
-            }
-        }
-    }
-
-    private foo(item: Item) {
-        if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-            }
-        }
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.sellIn = item.sellIn - 1;
-        }
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                if (item.quality > 0) {
                     item.quality = item.quality - 1
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1
+                    }
                 }
             }
         }
